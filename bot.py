64a1d061c815904d0f29139f4cacd3d827deed83
widziz <widziyt@gmail.com>
2025-06-04 18:58:11 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
+from config import BOT_TOKEN
 
-BOT_TOKEN = "7712265453:AAEbDMm6Xikg9vawNF1737AsrDG_FyKOsAQ"
-
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
