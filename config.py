49b2258c479927f3312382e4d5d3513563f43d75
widziz <widziyt@gmail.com>
2025-06-04 19:07:17 +0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_SECRET = "d50ef92a7c4d3bad7c453a3610e06869"
-WEBHOOK_PATH = f"/webhook/{WEBHOOK_SECRET}"
-WEBHOOK_URL = f"https://drain-5mb6.onrender.com{WEBHOOK_PATH}"
