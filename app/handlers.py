2bf38d5ccfc5b94cfbeab57c02f542c6dc054ec9
widziz <widziyt@gmail.com>
2025-06-06 15:56:08 +0400

    Update handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -9,7 +9,3 @@ async def business_handler(message: Message):
         text="Привет, это бизнес чат-бот!",
         business_connection_id=message.business_connection_id
     )
-
-@router.message()
-async def fallback_handler(message: Message):
-    await message.answer("Это обычный бот. Напишите нам через бизнес-аккаунт!")
