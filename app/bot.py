34f29b994f06687eb0fae15d7a0dc125cfd90341
widziz <widziyt@gmail.com>
2025-06-06 15:37:48 +0400

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -6,6 +6,12 @@ from aiogram.fsm.storage.memory import MemoryStorage
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
+
+async def ensure_webhook():
+    current = await bot.get_webhook_info()
+    if current.url != WEBHOOK_URL:
+        await bot.set_webhook(WEBHOOK_URL)
