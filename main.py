ebcb95d4797bb33e4c96e009471abcc467ac7791
widziz <widziyt@gmail.com>
2025-06-06 15:38:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import uvicorn
+import asyncio
 from app.webhook import app
+from app.bot import ensure_webhook
+
+# Установка вебхука при старте
+@app.on_event("startup")
+async def on_startup():
+    await ensure_webhook()
 
 if __name__ == "__main__":
     uvicorn.run("app.webhook:app", host="0.0.0.0", port=10000)
