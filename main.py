07e2d944dd816996e71cfca088114008a60dbf19
widziz <widziyt@gmail.com>
2025-06-06 15:34:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# main.py
 import uvicorn
 from app.webhook import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.webhook:app", host="0.0.0.0", port=8000)
-
-
+    uvicorn.run("app.webhook:app", host="0.0.0.0", port=10000)
