ece0aaa405c58407f7946ba033d0657d0fa3a9ce
widziz <widziyt@gmail.com>
2025-06-07 23:26:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import uvicorn
 from app.webhook import app
-from app.bot import ensure_webhook
-
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await ensure_webhook()
-    yield
-    await bot.session.close()
-
-app = FastAPI(lifespan=lifespan)
-
 
 if __name__ == "__main__":
     import os
